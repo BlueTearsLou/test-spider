892f785ff3b77153f026f5c3a0dbc1e525136961
BlueTearsLou <2460759244@qq.com>
2021-11-18 15:35:02 +0800

    添加一个测试文件

---
+++ msg.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+
+
+TOKEN_IS_NOT_VALID = '无效的token'
+USER_NOT_FOUND = '用户不存在'
+RESPONSE_TRUE = '请求成功'
+RESPONSE_FALSE = '请求失败'
+NAME_OR_PASSWORD_ERROR = '请确认账号权限或用户名和密码不正确'
+INTERNAL_ERROR = '服务器错误'
